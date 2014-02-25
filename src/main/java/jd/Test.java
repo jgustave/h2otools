bb3a51dec468a36fd36c821d2faf1435226c326c
jgustave <jerdavis@>
2014-02-24 18:09:24 -0800

    Just getting started

--- src/main/java/jd/Test.java
+++ src/main/java/jd/Test.java
@@ -1,10 +1,13 @@
 package jd;
 
+import water.H2O;
+
 /**
  *
  */
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        H2O.main(new String[]{});
     }
 }
